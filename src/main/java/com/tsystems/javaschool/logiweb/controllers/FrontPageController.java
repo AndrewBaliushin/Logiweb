5a2c00578891c089a9626de958ff6ce606e5c33c
Andrew Baliushin <uo4ever@mail.ru>
2015-07-19 18:02:06 +0300

    Common front page added

---
+++ src/main/java/com/tsystems/javaschool/logiweb/controllers/FrontPageController.java
@@ -0,0 +1,36 @@
+package com.tsystems.javaschool.logiweb.controllers;
+
+import java.util.Collection;
+
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.tsystems.javaschool.logiweb.entities.status.UserRole;
+
+@Controller
+@RequestMapping("/")
+public class FrontPageController {
+
+    @RequestMapping("*")
+    public String commonFrontPage() {
+        Collection<SimpleGrantedAuthority> authorities = (Collection<SimpleGrantedAuthority>) SecurityContextHolder
+                .getContext().getAuthentication().getAuthorities();
+
+        GrantedAuthority manager = new SimpleGrantedAuthority(
+                UserRole.ROLE_MANAGER.name());
+        GrantedAuthority driver = new SimpleGrantedAuthority(
+                UserRole.ROLE_DRIVER.name());
+
+        if (authorities.contains(manager)) {
+            return "forward:/manager";
+        } else if (authorities.contains(driver)) {
+            return "forward:/driver";
+        } 
+        
+        throw new AccessDeniedException("Your user role is unknown.");
+    }
+}
--- src/main/java/com/tsystems/javaschool/logiweb/controllers/ext/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Extensions and utils for presentation layer.
- * @author Andrey Baliushin
- *
- */
-package com.tsystems.javaschool.logiweb.controllers.ext;
